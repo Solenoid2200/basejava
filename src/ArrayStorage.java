b509ce0358cddf3d09bb3b7b656bc3277310bb7d
Sole Noid <solenoid2200@mail.ru>
2018-06-29 18:06:16 +0200

    Class ArrayStorage.java changed

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,67 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+
     Resume[] storage = new Resume[10000];
+    int storageCount = 0;
+
 
     void clear() {
+        Arrays.fill(storage, 0, storageCount, null);
+        storageCount = 0;
     }
+
 
     void save(Resume r) {
+        storage[storageCount] = r;
+        storageCount++;
     }
 
+
     Resume get(String uuid) {
+        for (int i = 0; i < storageCount; i++) {
+            if(storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
+
     void delete(String uuid) {
+        for (int i = 0; i < storageCount; i++) {
+
+            if(storage[i].uuid.equals(uuid)) {
+
+                storage[i] = null;
+
+                for (int j = i; j < storageCount; j++) {
+                    storage[j] = storage[j+1];
+                }
+
+                storageCount--;
+                break;
+
+            }
+        }
     }
+
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] result;
+        result = Arrays.copyOfRange(storage, 0, storageCount);
+        return result;
     }
 
+
     int size() {
-        return 0;
+        return storageCount;
     }
+
 }
