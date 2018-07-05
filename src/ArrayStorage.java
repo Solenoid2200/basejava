1cc6f4f5e86e79e0fb33b2fdef10a00ef5d86501
Sole Noid <solenoid2200@mail.ru>
2018-07-05 17:45:43 +0200

    method update changed

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -22,6 +22,7 @@ public class ArrayStorage {
         for (int i = 0; i < size; i++) {
                 if(storage[i].uuid.equals(resume.uuid)) {
                     storage[i] = resume;
+                    break;
                 }
         }
     }
