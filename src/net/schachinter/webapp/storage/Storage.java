367d7e38ae5c8c98e9f4e50b93d9ee3e78d12fca
Sole Noid <solenoid2200@mail.ru>
2018-07-12 13:37:58 +0200

    Code format

--- src/net/schachinter/webapp/storage/Storage.java
+++ src/net/schachinter/webapp/storage/Storage.java
@@ -2,16 +2,20 @@ package net.schachinter.webapp.storage;
 
 import net.schachinter.webapp.model.Resume;
 
-import java.util.Arrays;
-
 public interface Storage {
 
     void clear();
+
     void save(Resume resume);
+
     Resume get(String uuid);
+
     Resume[] getAll();
+
     void update(Resume resume);
+
     void delete(String uuid);
+
     int size();
 
 }
