fd382e20f6fb9cd441f2b51fd8c202810316fa7b
Sole Noid <solenoid2200@mail.ru>
2018-07-12 14:10:14 +0200

    Class SortedArrayStorage filled

--- src/net/schachinter/webapp/storage/SortedArrayStorage.java
+++ src/net/schachinter/webapp/storage/SortedArrayStorage.java
@@ -7,6 +7,58 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class SortedArrayStorage {
+public class SortedArrayStorage extends AbstractArrayStorage {
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void save(Resume resume) {
+        if (size > storage.length - 1) {
+            System.out.println("Array is filled!");
+            return;
+        }
+        if (getIndex(resume.getUuid()) != -1) {
+            System.out.println("Resume exist!");
+            return;
+        }
+        storage[size] = resume;
+        size++;
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, size);
+    }
+
+    public void update(Resume resume) {
+        int index = getIndex(resume.getUuid());
+        if (index != -1) {
+            storage[index] = resume;
+            return;
+        }
+        System.out.println("Resume not exist!");
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index != -1) {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+            return;
+        }
+        System.out.println("Resume not exist!");
+    }
+
+    @Override
+    protected int getIndex(String uuid) {
+        Resume searchKey = new Resume();
+        searchKey.setUuid(uuid);
+        return Arrays.binarySearch(storage, 0, size, searchKey);
+    }
 
 }
