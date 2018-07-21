7490c9342f91083ab583b3af7e26f4238d40b046
Sole Noid <solenoid2200@mail.ru>
2018-07-21 22:47:30 +0200

    Package test added

---
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,41 @@
+package ru.javawebinar.basejava.storage;
+
+import org.junit.Test;
+import ru.javawebinar.basejava.model.Resume;
+
+import static org.junit.Assert.*;
+
+public class AbstractArrayStorageTest {
+
+    Resume r1 = new Resume();
+    r1.setUuid("uuid1");
+
+    @Test
+    public void size() throws Exception {
+    }
+
+    @Test
+    public void clear() throws Exception {
+    }
+
+    @Test
+    public void update() throws Exception {
+    }
+
+    @Test
+    public void getAll() throws Exception {
+    }
+
+    @Test
+    public void save() throws Exception {
+    }
+
+    @Test
+    public void delete() throws Exception {
+    }
+
+    @Test
+    public void get() throws Exception {
+    }
+
+}
