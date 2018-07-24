620361164f95d349f39d0b9dc75d599c1593461d
Sole Noid <solenoid2200@mail.ru>
2018-07-24 22:09:04 +0200

    Code format

--- test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
@@ -1,8 +1,5 @@
 package ru.javawebinar.basejava.storage;
 
-import org.junit.Assert;
-import ru.javawebinar.basejava.model.Resume;
-
 public class ArrayStorageTest extends AbstractArrayStorageTest {
     public ArrayStorageTest() {
         super(new ArrayStorage());
