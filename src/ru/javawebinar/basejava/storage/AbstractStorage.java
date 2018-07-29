ed65e9b4da02072265d0a3a3c035726c0559d9ff
Sole Noid <solenoid2200@mail.ru>
2018-07-29 20:20:47 +0200

    Move variable STORAGE_LIMIT, size to class AbstractArrayStorage

--- src/ru/javawebinar/basejava/storage/AbstractStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractStorage.java
@@ -1,25 +1,9 @@
 package ru.javawebinar.basejava.storage;
 
-import ru.javawebinar.basejava.exception.ExistStorageException;
 import ru.javawebinar.basejava.exception.NotExistStorageException;
-import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
 
 public abstract class AbstractStorage implements Storage {
-    static final int STORAGE_LIMIT = 10000;
-    protected int size = 0;
-
-    public void save(Resume resume) {
-        int index = getIndex(resume.getUuid());
-        if (index >= 0) {
-            throw new ExistStorageException(resume.getUuid());
-        } else if (size == STORAGE_LIMIT) {
-            throw new StorageException("Storage overflow", resume.getUuid());
-        } else {
-            insertElement(resume, index);
-            size++;
-        }
-    }
 
     public void update(Resume resume) {
         updateIndex(checkIndex(resume.getUuid()), resume);
@@ -27,15 +11,10 @@ public abstract class AbstractStorage implements Storage {
 
     public void delete(String uuid) {
         deleteDeletedElement(checkIndex(uuid), uuid);
-        size--;
     }
 
     public Resume get(String uuid) {
         return getResume(checkIndex(uuid), uuid);
-    }
-
-    public int size() {
-        return size;
     }
 
     private int checkIndex(String uuid) {
@@ -46,18 +25,18 @@ public abstract class AbstractStorage implements Storage {
         return index;
     }
 
+    public abstract void save(Resume resume);
+
     public abstract void clear();
 
     public abstract Resume[] getAll();
-
-    protected abstract int getIndex(String uuid);
-
-    protected abstract void insertElement(Resume resume, int index);
 
     protected abstract void updateIndex(int index, Resume resume);
 
     protected abstract void deleteDeletedElement(int index, String uuid);
 
     protected abstract Resume getResume(int index, String uuid);
+
+    protected abstract int getIndex(String uuid);
 }
 
