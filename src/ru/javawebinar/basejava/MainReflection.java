0869100975410d86f96440fcc96850f8b5ee3826
Sole Noid <solenoid2200@mail.ru>
2018-07-31 23:59:56 +0200

    Class MainReflection changed

--- src/ru/javawebinar/basejava/MainReflection.java
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -2,7 +2,6 @@ package ru.javawebinar.basejava;
 
 import ru.javawebinar.basejava.model.Resume;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -10,9 +9,7 @@ public class MainReflection {
 
     public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume resume = new Resume();
-        Field field = resume.getClass().getDeclaredFields()[0];
-        field.setAccessible(true);
-        field.set(resume, "test_uuid");
+        System.out.println(resume);
         Class<? extends Resume> clazz = resume.getClass();
         Method method = clazz.getMethod("toString");
         System.out.println(method.invoke(resume));
