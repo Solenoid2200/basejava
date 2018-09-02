55a90fd27943045d1fe75859d5c62fc73d7ad20a
Sole Noid <solenoid2200@mail.ru>
2018-09-02 22:33:13 +0200

    8.2 -- In package util: Class FileNames added

---
+++ src/ru/javawebinar/basejava/util/FileNames.java
@@ -0,0 +1,20 @@
+package ru.javawebinar.basejava.util;
+
+import java.io.File;
+import java.util.Objects;
+
+public class FileNames {
+    public static void main(String[] args) {
+        printAllFiles(new File("c:/basejava"));
+    }
+
+    private static void printAllFiles(File directory) {
+        for (File file : Objects.requireNonNull(directory.listFiles())) {
+            if (file.isDirectory()) {
+                printAllFiles(file);
+            } else {
+                System.out.println(file.getAbsolutePath());
+            }
+        }
+    }
+}
