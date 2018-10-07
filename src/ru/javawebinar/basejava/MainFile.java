9dbbd212ba1ace344b2fdfe2763ee36f50d27603
Sole Noid <solenoid2200@mail.ru>
2018-10-07 19:02:51 +0200

    -- HW9.1 : File MainFile added

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -1,54 +1,36 @@
 package ru.javawebinar.basejava;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 
-/**
- * gkislin
- * 21.07.2016
- */
 public class MainFile {
     public static void main(String[] args) {
-        String filePath = ".\\.gitignore";
-
-        File file = new File(filePath);
-        try {
-            System.out.println(file.getCanonicalPath());
-        } catch (IOException e) {
-            throw new RuntimeException("Error", e);
-        }
-
         File dir = new File("./src/ru/javawebinar/basejava");
-        System.out.println(dir.isDirectory());
-        String[] list = dir.list();
-        if (list != null) {
-            for (String name : list) {
-                System.out.println(name);
-            }
-        }
-
-        try (FileInputStream fis = new FileInputStream(filePath)) {
-            System.out.println(fis.read());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        printDirectoryDeeply(dir);
+        printDirectoryDeeply(dir, 0);
     }
 
-    // TODO: make pretty output
-    public static void printDirectoryDeeply(File dir) {
+    public static void printDirectoryDeeply(File dir, int count) {
         File[] files = dir.listFiles();
 
         if (files != null) {
             for (File file : files) {
                 if (file.isFile()) {
-                    System.out.println("File: " + file.getName());
+                    String st = createIndent(count);
+                    System.out.println(st + "File: " + file.getName());
                 } else if (file.isDirectory()) {
-                    System.out.println("Directory: " + file.getName());
-                    printDirectoryDeeply(file);
+                    String st = createIndent(count);
+                    System.out.println(st + "Directory: " + file.getName());
+                    printDirectoryDeeply(file, count+4);
                 }
             }
         }
     }
+
+    private static String createIndent(int count) {
+        String result = "";
+        for (int i = 0; i < count; i++) {
+            result = result + " ";
+        }
+        return result;
+    }
+
 }
