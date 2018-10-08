e7dc66ec4589aa8aeb5fbb14109f7001417b76b6
Sole Noid <solenoid2200@mail.ru>
2018-10-08 13:41:40 +0200

    HW 9.1 Class MainFile is changed

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -5,32 +5,20 @@ import java.io.File;
 public class MainFile {
     public static void main(String[] args) {
         File dir = new File("./src/ru/javawebinar/basejava");
-        printDirectoryDeeply(dir, 0);
+        printDirectoryDeeply(dir, "    ");
     }
 
-    public static void printDirectoryDeeply(File dir, int count) {
+    public static void printDirectoryDeeply(File dir, String indent) {
         File[] files = dir.listFiles();
 
         if (files != null) {
             for (File file : files) {
-                if (file.isFile()) {
-                    String st = createIndent(count);
-                    System.out.println(st + "File: " + file.getName());
-                } else if (file.isDirectory()) {
-                    String st = createIndent(count);
-                    System.out.println(st + "Directory: " + file.getName());
-                    printDirectoryDeeply(file, count+4);
+                if (file.isDirectory()) {
+                    System.out.println(indent + "Directory: " + file.getName());
+                    printDirectoryDeeply(file, indent + "    ");
                 }
             }
         }
-    }
-
-    private static String createIndent(int count) {
-        String result = "";
-        for (int i = 0; i < count; i++) {
-            result = result + " ";
-        }
-        return result;
     }
 
 }
